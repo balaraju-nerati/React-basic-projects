f39c1380f5725bc4f96cc055a1ba88bf8bbc9ab4
balaraju-nerati <balrajnerati23@gmail.com>
2025-02-03 20:25:34 +0530

    project built using mealDB api

---
+++ 3.Meals-API/App.jsx
@@ -0,0 +1,13 @@
+import Meals from "./components/Meals"
+
+const App = () => {
+  return (
+    <div>
+      {/* <Counter/> */}
+      {/* <Todolist/> */}
+      <Meals/>
+    </div>
+  )
+}
+
+export default App
